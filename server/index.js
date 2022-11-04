326951afbf5fe040697a2fc704d1ad5c03cacef6
bfelda <bfelda@gmail.com>
2022-11-03 22:18:28 -0500

    wrong file

--- server/index.js
+++ server/index.js
@@ -5,12 +5,19 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+	res.json({ message: "Hello from the server!" });
+});
+
 app.get("/", (req, res) => {
 	console.log("Request for root......................................");
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.post("/sign", (req, res) => {
+	console.log("inside the sign request handle");
 	return res.redirect("/");
 });
 
