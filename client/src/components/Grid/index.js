f9dd8712ceb3d217aa3dc2cdc8b1e423d7f71e51
bfelda <bfelda@gmail.com>
2022-11-03 22:14:02 -0500

    updated

---
+++ client/src/components/Grid/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { getRefreshSignedRequest, decode } from "./utils/canvasUtil";
+import "@salesforce/canvas-js-sdk";
+
+export default function Grid() {
+	const [sr, setSr] = useState();
+
+	useEffect(() => {
+		populateSignedRequest();
+	});
+
+	const populateSignedRequest = () => {
+		getRefreshSignedRequest().then((data) => {
+			let payload = data.payload.response;
+			// TODO  Add in the validation.  Get the consumer key from env and decode the payload[0] to verify authenticity
+			let part = payload.split(".")[1];
+			let signedRequest = decode(part);
+			let signedRequestJSON = JSON.parse(signedRequest);
+			setSr(signedRequestJSON);
+			console.log(signedRequest);
+		});
+	};
+	return <div>grid</div>;
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Grid from "./components/Grid";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Grid />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
